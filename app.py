6ad319a396d41b2611c2e015e40debfd6a987536
Ana Kezia <108835900+kezia0310@users.noreply.github.com>
2023-08-15 02:05:03 +0000

    criando rotas

--- app.py
+++ app.py
@@ -6,7 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template('acesso.html')
 
+@app.route('/cadastro')
+def cadastro():
+  return render_template('cadastro.html')
 
+@app.route('/competicoes')
+def cadastro():
+  return render_template('competicoes.html')
 
 
 app.run(host='0.0.0.0', port=81)
